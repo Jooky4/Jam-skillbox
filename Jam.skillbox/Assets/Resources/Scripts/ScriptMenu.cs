d4924720c2100d559fdd682fc0abd10dcfc471fe
Izillger <44162880+Izillger@users.noreply.github.com>
2020-11-17 01:03:11 +0700

    MainMenu
    Основа меню готова, скрипт запуска  сцены на кнопке пока отключен, кнопка выхода должна работать.

---
+++ Jam.skillbox/Assets/Resources/Scripts/ScriptMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptMenu : MonoBehaviour
+{
+    public void onCklickStartGame() // Запуск игры
+    {
+        Application.LoadLevel(name: "MainScene"); // Запуск игровой сцены
+    }
+
+    public void onCklickExitGame() // Выход из игры при нажатии кнопки в меню
+    {
+        Application.Quit();  // Закрыть приложение
+    }
+
+    void Update() // Завершает работу приложения при нажатии на кнопку Esc
+    {
+        if (Input.GetKey("escape")) // если нажата кнопка Esc (Escape)
+        {
+            Application.Quit();  // Закрыть приложение
+        }
+    }
+}
